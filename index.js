583320f96ecd7b2f1988f917bbc33ae5051f8c90
Florchu94 <florencia_box@hotmail.com>
2025-05-28 00:01:28 -0300

    se modificó boton

---
+++ index.js
@@ -0,0 +1,18 @@
+//* Boton Volver Arriba //
+
+const btnVolverArriba = document.getElementById('btnVolverArriba');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    btnVolverArriba.style.display = 'block';
+  } else {
+    btnVolverArriba.style.display = 'none';
+  }
+});
+
+btnVolverArriba.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
