bba4bb0378f4a1d7a43bb38fde20f34d0f07f450
Florchu94 <florencia_box@hotmail.com>
2025-06-15 20:26:10 -0300

    Se añadió tempData

---
+++ scripts/funciones.js
@@ -0,0 +1,6 @@
+console.log(tempData);
+
+const Container = document.querySelector('.Container-Cards');
+tempData.forEach(movie => {
+  console.log('movie', movie);
+});
---
+++ scripts/tempData.js
@@ -0,0 +1,20 @@
+const tempData = [
+  {
+    title: 'Barbie',
+    year: 2023,
+    director: 'Peter Jackson',
+    duration: '1:54:00',
+    genre: ['Feminism', 'Comedy', 'Fantasy'],
+    rate: 4.9,
+    poster: 'assets/Poster-Barbie.png',
+  },
+  {
+    title: [],
+    year:
+    director: [],
+    duration: 
+    genre: [],
+    rate: [],
+    poster: [],
+  },
+];
